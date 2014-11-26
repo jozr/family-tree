fe1ab8649b4c3ac565441546132a6654dc2a4197
jozwright <jozwright@gmail.com>
2014-11-25 23:25:19 -0800

    WIP

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,16 @@
-var app = angular.module("app", []).config(function($routeProvider) {
+var app = angular.module("app", [])
+
+app.config(function($routeProvider) {
+
 	$routeProvider.when('/login', {
 		templateUrl: 'login.html',
 		controller: 'LoginController'
 	});
+
+	$routeProvider.otherwise({ redirectTo : '/login' });
+	
 });
 
+app.controller('LoginController', function() {
+
+});
